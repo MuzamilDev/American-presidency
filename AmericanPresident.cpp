29befea11e8ad78189ccf352eb99e2289df32b0e
Muzamil Zaman Khan <95095602+MuzamilDev@users.noreply.github.com>
2022-10-29 12:24:06 +0500

    Add files via upload

---
+++ AmericanPresident.cpp
@@ -0,0 +1,166 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int tenure;
+
+    cout<<"Enter Year "<<endl;
+    cin>>tenure;
+
+    switch(tenure)
+    {
+
+    case 1933:
+    case 1934:
+    case 1935:
+    case 1936:
+    case 1937:
+    case 1938:
+    case 1939:
+    case 1940:
+    case 1941:
+    case 1942:
+    case 1943:
+    case 1944:
+
+        cout<<" Franklin D Roosevelet 1933--1945 "<<endl;
+        break;
+
+    case 1945:
+    case 1946:
+    case 1947:
+    case 1948:
+    case 1949:
+    case 1950:
+    case 1951:
+    case 1952:
+
+        cout<<"Harry S Trumen 1945--1953 "<<endl;
+        break;
+
+    case 1953:
+    case 1954:
+    case 1955:
+    case 1956:
+    case 1957:
+    case 1958:
+    case 1959:
+    case 1960:
+
+        cout<<"Dwight D Eisenhower 1953--1961"<<endl;
+        break;
+
+    case 1961:
+    case 1962:
+
+        cout<<"John F Kennedy 1961--1963"<<endl;
+        break;
+
+    case 1963:
+    case 1964:
+    case 1965:
+    case 1966:
+    case 1967:
+    case 1968:
+
+        cout<<"Lyndon b Johnson 1963--1969"<<endl;
+        break;
+
+    case 1969:
+    case 1970:
+    case 1071:
+    case 1972:
+    case 1973:
+
+        cout<<"Richard M Nixon 1969--1974 "<<endl;
+        break;
+
+    case 1974:
+    case 1975:
+    case 1976:
+
+        cout<<"Gerald  R Ford 1974--1977"<<endl;
+        break;
+
+    case 1977:
+    case 1978:
+    case 1979:
+    case 1980:
+
+        cout<<"Jimmy Carter 1977--1981"<<endl;
+        break;
+
+    case 1981:
+    case 1982:
+    case 1983:
+    case 1984:
+    case 1985:
+    case 1986:
+    case 1987:
+    case 1988:
+
+        cout<<"Ronald Reagan 1981--1989"<<endl;
+        break;
+
+    case 1989:
+    case 1990:
+    case 1991:
+    case 1992:
+
+        cout<<"George H W Bush 1989--1993"<<endl;
+        break;
+
+    case 1993:
+    case 1994:
+    case 1995:
+    case 1996:
+    case 1997:
+    case 1998:
+    case 1999:
+    case 2000:
+
+        cout<<"William J Clinton 1993--2001"<<endl;
+        break;
+
+    case 2001:
+    case 2002:
+    case 2003:
+    case 2004:
+    case 2005:
+    case 2006:
+    case 2007:
+    case 2008:
+
+        cout<<"George W Bush 2001-2009"<<endl;
+        break;
+
+    case 2009:
+    case 2010:
+    case 2011:
+    case 2012:
+    case 2013:
+    case 2014:
+    case 2015:
+    case 2016:
+
+        cout<<"Barack Obama 2009--2017"<<endl;
+        break;
+
+    case 2017:
+    case 2018:
+    case 2019:
+    case 2020:
+
+        cout<<"Donald Trump 2017--2021"<<endl;
+        break;
+
+    case 2021:
+    case 2022:
+        cout<<"Biden is present "<<endl;
+        break;
+
+    }
+    return 0;
+
+}
